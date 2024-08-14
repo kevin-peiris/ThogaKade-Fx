4e21abaa5899b0b6f7fb1b7d3216ec63e44a3de6
kevin-peiris <kevin.2007.01.22@gmail.com>
2024-08-14 11:35:17 +0530

    Generated customer Id

--- src/main/java/controller/AddCustomerFormController.java
+++ src/main/java/controller/AddCustomerFormController.java
@@ -2,17 +2,29 @@ package controller;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import db.ThogakadePOS;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
+import model.Customer;
 
-public class AddCustomerFormController {
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class AddCustomerFormController implements Initializable {
+    private static int customerCount=1;
 
     @FXML
     private DatePicker dob;
 
     @FXML
-    private JFXComboBox<?> genderCombo;
+    private JFXComboBox<String> comboTitles;
 
     @FXML
     private JFXTextField txtAddress;
@@ -29,6 +41,32 @@ public class AddCustomerFormController {
     @FXML
     void btnAddCustomerOnAction(ActionEvent event) {
 
+        Customer customer = new Customer(txtId.getText(), comboTitles.getValue(), txtName.getText(), txtAddress.getText(), txtPhoneNumber.getText(), dob.getValue());
+        System.out.println(customer);
+
+        new ThogakadePOS().getInstance().addCustomer(customer);
+
+        customerCount++;
+        generateCusId();
+        txtName.setText("");
+        txtAddress.setText("");
+        txtPhoneNumber.setText("");
+        comboTitles.setValue("");
+        dob.setValue(null);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { //Adds the values to the tile ComboBox
+        ObservableList<String> titles= FXCollections.observableArrayList();
+        titles.add("Mr.");
+        titles.add("Mrs.");
+        comboTitles.setItems(titles);
+
+        generateCusId();
+    }
+
+    private void generateCusId(){ //Generates the customer Id
+        String id=String.format("C%03d",customerCount);
+        txtId.setText(id);
+    }
 }
